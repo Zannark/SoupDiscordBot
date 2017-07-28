a90118c0fdc90e3effe7d77beb2f15b409ba8240
Zannark <sherbet.gray54@googlemail.com>
2017-07-28 14:24:10 +0100

    Added darth detector

--- Src/PlaylistBot.py
+++ Src/PlaylistBot.py
@@ -50,6 +50,10 @@ async def on_message(message):
     if "CASUAL" in message.content.upper() and Client.user.name != message.author.name:
         await Client.send_message(message.channel, "ewwwwwwww @everyone, " + message.author.name + " is a filthy casual")
         await Client.send_file(message.channel, "Data//Casuals.png")
+
+    IsDarth = ("PLAGUEIS" in message.content.upper() or "DARTH" in message.content.upper())
+    if IsDarth and Client.user.name != message.author.name:
+        await Client.send_message(message.channel, "Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.")
     
     if message.content.startswith('.summon'):
         C = Client.get_channel(ChannelID)
